d10c3fa4a6e16a1362a8e70868adb8629fcadbb2
shivam <shivamsoni28feb2002@gmail.com>
2023-11-19 00:06:56 +0530

    K pattern

---
+++ k.java
@@ -0,0 +1,11 @@
+public class k {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1;i<=9;i++){
+			for(j=1;j<=(6-i) || j<=(i-4);j++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
