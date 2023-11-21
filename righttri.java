be7b300047c2dec8e45ab51258d4709351f4dd57
shivam <shivamsoni28feb2002@gmail.com>
2023-11-21 14:56:04 +0530

    right triangle pattern

---
+++ righttri.java
@@ -0,0 +1,13 @@
+public class righttri {
+	public static void main(String[]args){
+		int i,j;
+		for(i=1;i<=5;){
+			for(j=1;j<=i;){
+				System.out.print("* ");
+				j++;
+			}
+			i++;
+			System.out.println("\n");
+		}
+	}
+}
