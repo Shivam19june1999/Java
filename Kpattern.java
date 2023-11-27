37081d7946c5fc101132e33c054279822d23d58e
shivam <shivamsoni28feb2002@gmail.com>
2023-11-27 22:21:05 +0530

    Dynamic K pattern

--- Kpattern.java
+++ Kpattern.java
@@ -1,13 +1,14 @@
 public class Kpattern {
 	public static void main(String[] args) {
-		int r =9;
-		// int i,j;
-		// for(i=1;i<=9;i++){
-		// for(j=1;j<=(6-i) || j<=(i-4);j++){
-		// System.out.print("*");
-		// }
-		// System.out.print("\n");
-		// }
+		int n = 11;
+		int h=(n+1)/2;
+		int i,j;
+		for(i=1;i<=n;i++){
+		for(j=1;j<=(h-i+1) || j<=(i-h+1);j++){
+		System.out.print("* ");
+		}
+		System.out.print("\n");
+		}
 		
         // for (int i = 0; i < r; i++) {
         //     for (int j = i; j <=(r / 2); j++) {
