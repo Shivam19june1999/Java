4bc4d565a1f0c2965e34547f134f915b41278db7
shivam <shivamsoni28feb2002@gmail.com>
2023-12-10 21:11:26 +0530

    created index based matrix

---
+++ TwoDArray/index.java
@@ -0,0 +1,24 @@
+//particular index print
+package TwoDArray;
+public class index {
+
+	public static void main(String[] args) {
+		int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr[i].length;j++){
+				// if((i+j)%2==0){
+					if(i==1 && j==1)
+					{
+						// System.out.print(" ");
+						System.out.print(arr[i][j]+" ");
+					}
+					else
+					{
+						continue;
+					}
+				// }
+			}
+			// System.err.println();
+		}
+	}
+}
